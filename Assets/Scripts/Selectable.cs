9358a3dd90faa92a30d2ca53f7041f9b0f4dbaf8
Yura_Kotkevich <71070351+YuraKotkevich@users.noreply.github.com>
2021-02-18 20:14:56 +0200

    US-1 Maining

---
+++ Assets/Scripts/Selectable.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Selectable : MonoBehaviour
+{
+    public void Select()
+    {
+        GetComponent<Renderer>().material.color = Color.yellow;
+        if (Input.GetKeyDown("e"))
+        {
+            Collect.Money += 5;
+            Destroy(gameObject);
+        }
+    }
+
+    public void Deselect()
+    {
+        GetComponent<Renderer>().material.color = Color.black;
+    }
+
+}
