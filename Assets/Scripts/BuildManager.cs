e5cd3157200c08a018667bd2f4a2ca1da011105b
taras1808 <taras1808@gmail.com>
2021-02-19 19:17:54 +0100

    US-3 Building system

---
+++ Assets/Scripts/BuildManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+    public GameObject foundation;
+
+    public GameObject wall;
+
+    public GameObject ceiling;
+
+
+    public BuildSystem buildSystem;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.H) && !buildSystem.isBuilding)
+        {
+            buildSystem.NewBuild(foundation);
+        }
+
+        if (Input.GetKeyDown(KeyCode.J) && !buildSystem.isBuilding)
+        {
+            buildSystem.NewBuild(wall);
+        }
+
+        if (Input.GetKeyDown(KeyCode.K) && !buildSystem.isBuilding)
+        {
+            buildSystem.NewBuild(ceiling);
+        }
+    }
+}
---
+++ Assets/Scripts/Preview.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Preview : MonoBehaviour
+{
+
+    public GameObject prefab;
+
+    private MeshRenderer renderer;
+    public Material goodMat;
+    public Material badMat;
+
+    private BuildSystem buildSystem;
+
+    public bool isSnapped { get; private set; }
+    public bool isFoundation = false;
+
+    public List<string> tagsSnapTo = new List<string>();
+
+    private void Start()
+    {
+        buildSystem = FindObjectOfType<BuildSystem>();
+        renderer = GetComponent<MeshRenderer>();
+        ChangeColor();
+    }
+
+    public void Place()
+    {
+        Instantiate(prefab, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+
+    private void ChangeColor()
+    {
+        if (isSnapped)
+        {
+            renderer.material = goodMat;
+        }
+        else
+        {
+            renderer.material = badMat;
+        }
+
+        if (isFoundation)
+        {
+            renderer.material = goodMat;
+            isSnapped = true;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        for (int i = 0; i < tagsSnapTo.Count; i++)
+        {
+            string currentTag = tagsSnapTo[i];
+            if (other.tag == currentTag)
+            {
+                buildSystem.PauseBuild(true);
+                transform.position = other.transform.position;
+                isSnapped = true;
+                ChangeColor();
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        for (int i = 0; i < tagsSnapTo.Count; i++)
+        {
+            string currentTag = tagsSnapTo[i];
+            if (other.tag == currentTag)
+            {
+                isSnapped = false;
+                ChangeColor();
+            }
+        }
+    }
+
+}
