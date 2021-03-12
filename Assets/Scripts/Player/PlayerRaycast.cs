5d9bcd4779812cd1edaa804f59152463497ea57c
Yura_Kotkevich <71070351+YuraKotkevich@users.noreply.github.com>
2021-03-12 20:55:20 +0200

    US-8 Cannon charging

--- Assets/Scripts/Player/PlayerRaycast.cs
+++ Assets/Scripts/Player/PlayerRaycast.cs
@@ -8,36 +8,48 @@ public class PlayerRaycast : MonoBehaviour
     [SerializeField]
     private LayerMask RaycastLayer = ~0;
 
+    public GameObject showEnergyCannon;
+    public GameObject showEnergyGenerator;
+
     void Update()
     {
+        showEnergyCannon.SetActive(false);
+        showEnergyGenerator.SetActive(false);
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
         if (Physics.Raycast(ray, out hit, RaycastDistance, RaycastLayer, QueryTriggerInteraction.Ignore))
         {
             Transform t = hit.transform;
 
-            bool isMain = false;
             while (t.parent != null)
             {
                 t = t.parent;
-                if (t.parent == null)
-                {
-                    isMain = true;
-                    break;
-                }
             }
-            if (isMain)
+            
+            EnergyGenerator generator = t.GetComponent<EnergyGenerator>();
+            CannonEnergy energy = t.GetComponent<CannonEnergy>();
+
+            if (generator)
             {
-                EnergyGenerator generator = t.GetComponent<EnergyGenerator>();
-                
-                if (Input.GetKeyDown(KeyCode.E))
+                showEnergyGenerator.SetActive(true);
+            }
+            else if (energy)
+            {
+                showEnergyCannon.SetActive(true);
+            }
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                if (generator)
                 {
-                    if (generator)
-                    {
-                        generator.GetEnergy(25);
-                    }
+                    generator.GetEnergy(25);
+                }
+                else if (energy)
+                {
+                    energy.Charge(25);
                 }
             }
+    
         }
     }
 }
